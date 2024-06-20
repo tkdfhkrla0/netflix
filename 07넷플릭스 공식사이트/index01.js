fa2b9cf06ded7192c249893e6a75b5025099bdb5
tkdfhkrla0 <164144021+tkdfhkrla0@users.noreply.github.com>
2024-06-20 22:43:22 +0900

    Add files via upload

---
+++ 07넷플릭스 공식사이트/index01.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.sign-in').click(function(){
+        $('.modal-login').fadeIn();
+    })
+    $('.close-btn').click(function(){
+        $('.modal-login').fadeOut();
+    })
+})
