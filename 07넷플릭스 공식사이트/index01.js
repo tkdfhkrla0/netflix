d84abfaa39f17be030bd4f0836a8498f6bb74b85
tkdfhkrla0 <164144021+tkdfhkrla0@users.noreply.github.com>
2024-06-20 22:48:25 +0900

    Delete 07넷플릭스 공식사이트 directory

--- 07넷플릭스 공식사이트/index01.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.sign-in').click(function(){
-        $('.modal-login').fadeIn();
-    })
-    $('.close-btn').click(function(){
-        $('.modal-login').fadeOut();
-    })
-})
